d983e79ec9c6b6472d68a97399df329015dc7117
edsagit <57862867+edsagit@users.noreply.github.com>
2021-11-10 12:43:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,29 @@
 
 // console.log("Hello, world!");
 
-AFRAME.registerComponent('markerhandler', {
-    init: function () {
-      this.el.sceneEl.addEventListener('markerFound', () => {
-        // redirect to custom URL e.g. google.com
-        console.log("Marker found");
-      })
-    }
-  });
+window.addEventListener('load', () => {
+    const camera = document.querySelector('[camera]');
+    const marker = document.querySelector('a-marker');
+   
+    camera.addEventListener('camera-error', () = {
+        console.log("Error acessing or loading the camera.");
+    })
+    
+    marker.addEventListener('markerFound', () => {
+        console.log("Marker found!")
+    });
+
+    marker.addEventListener('markerLost', () => {
+      clearInterval(check);
+    });
+})
+
+
+//AFRAME.registerComponent('markerhandler', {
+//    init: function () {
+//      this.el.sceneEl.addEventListener('markerFound', () => {
+//        // redirect to custom URL e.g. google.com
+//        console.log("Marker found");
+//      })
+//    }
+//  });
