29d4941c8271fc032f88190874422fa8867f62a4
Eduardo Araújo <araujoeduardo@protonmail.com>
2021-11-29 12:30:00 +0000

    Added MetalSynth, MetalSynth count and MetalSynth freq

--- js/synth.js
+++ js/synth.js
@@ -8,10 +8,27 @@
 //       release: 1 // 1s release
 //     }
 // }).toMaster();
+
 const now = Tone.now();
 const plucky = new Tone.PluckSynth().toDestination();
+
 let loopBeat;
-let membraneSynth;
+
+const membraneSynth = new Tone.MembraneSynth().toMaster();
+const metalSynth = new Tone.MetalSynth(
+  {
+    frequency : 200 ,
+    envelope : {
+      attack : 0.001 ,
+      decay : 0.1 ,
+      release : 0.01
+    },
+    harmonicity : 5.1 ,
+    modulationIndex : 32 ,
+    resonance : 4000 ,
+    octaves : 1.5
+    }          
+).toMaster();
 
 let markerBPM;
 let markerBPMValue;
@@ -19,9 +36,14 @@ let bpm;
 
 let markerFREQ;
 let markerFREQValue;
+
+let count;
 let freq;
+let freq2;
 
 let markerVisible = { markerBPM: false, m2: false };
+
+let counter;
 
 AFRAME.registerComponent('membrane', {
    // dependencies: ['raycaster'],  // for oculus go laser controls
@@ -40,10 +62,19 @@ AFRAME.registerComponent('membrane', {
   
     init: function () {
 
+      counter = 0;
+
+      // console.log(this.data.duration);
+
       let marker = document.querySelectorAll('a-marker');
 
       markerMembrane = document.querySelector('#membrane');
       markerMenbraneValue = document.querySelector('#membraneValue');
+
+      markerMetal = document.querySelector('#metal');
+      markerMetalValue = document.querySelector('#metalValue');
+      marker_Metal_Count = document.querySelector('#metalCount');
+      marker_Metal_FREQ = document.querySelector('#metalFreq');
 
       markerBPM = document.querySelector('#membraneBPM');
       markerBPMValue = document.querySelector('#membraneBPMValue');
@@ -52,10 +83,6 @@ AFRAME.registerComponent('membrane', {
       markerFREQ = document.querySelector('#membraneFREQ');
       markerFREQValue = document.querySelector('#membraneFREQValue');
       markerFREQRing = document.querySelector('#membraneFREQRing');
-
-      membraneSynth = new Tone.MembraneSynth().toMaster();
-      loopBeat = new Tone.Loop(song, '4n');
-      Tone.Transport.start();
 
       marker.forEach(m => m.addEventListener('markerFound', function () {
         console.log('FOUND: ' + m.id);
@@ -66,6 +93,10 @@ AFRAME.registerComponent('membrane', {
         console.log('LOST: ' + m.id);
         if (m.id == 'membrane') { loopBeat.stop(0);}
       }));
+
+
+      loopBeat = new Tone.Loop(song, '16n');
+      Tone.Transport.start();
     },
   
     trigger: function () {
@@ -79,26 +110,46 @@ AFRAME.registerComponent('membrane', {
   
     tick: function (time, timeDelta) {
 
-      bpm = Math.abs(map(markerBPM.object3D.rotation.y, -1.5, 1.5, 40, 300)); // Map marker Y axis rotation
-      freq = Math.abs(map(markerFREQ.object3D.rotation.y, -1.5, 1.5, 0, 1500)); // Map marker Y axis rotation
+      bpm = Math.abs(map(markerBPM.object3D.rotation.y, -1.5, 1.5, 40, 300)); // Map marker Y axis rotation to bpm
+      freq = Math.abs(map(markerFREQ.object3D.rotation.y, -1.5, 1.5, 0, 1500)); // Map marker Y axis rotation to freq
+      freq2 = Math.abs(map(marker_Metal_FREQ.object3D.rotation.y, -1.5, 1.5, 50, 5000));// Map marker Y axis rotation to freq
+      count = Math.abs(map(marker_Metal_Count.object3D.rotation.y, -1.5, 1.5, 0, 16));
+      // console.log(freq2);
 
       Tone.Transport.bpm.value = bpm; // Assign rotation value from marker to Tone BPM
 
       markerMenbraneValue.setAttribute('value', 'Membrane'); // Assign a-text membrane value
+      markerMetalValue.setAttribute('value', 'Metal'); // Assign a-text metal value
+
       markerBPMValue.setAttribute('value', 'BPM\n' + Math.round(bpm)); // Assign a-text BPM value
       markerBPMRing.setAttribute('theta-length', map(Math.round(bpm), 40, 300, 0, 360)); // Assign a-ring BPM theta value
+
       markerFREQValue.setAttribute('value', 'FREQ\n' + Math.round(freq)); // Assign a-text FREQ value
       markerFREQRing.setAttribute('theta-length', map(Math.round(freq), 0, 1500, 0, 360)); // Assign a-ring FREQ theta value
+    
+
+    
     },
 });
 
 function song(time) {
   
-  membraneSynth.triggerAttackRelease(freq, '8n', time);
-  // plucky.triggerAttack("C4", now);
+  if (counter%8 === 0) {
+    membraneSynth.triggerAttackRelease(freq, '8n', time, 1);
+    
+  }
+
+  if (counter%Math.round(count) === 0) {
+    // console.log("hello world");
+    plucky.triggerAttackRelease(freq2, time);
+    // metalSynth.triggerAttackRelease('8n', time+0.02, 1);
+  }
+
   // plucky.triggerAttack("C3", now + 1);
   // plucky.triggerAttack("C2", now + 1.5);
   // plucky.triggerAttack("C1", now + 2);
+  counter = (counter+1)%16;
+  // console.log(counter);
 }
 
 function map (number, inMin, inMax, outMin, outMax) {
