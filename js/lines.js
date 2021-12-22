97b2bd46a68d2f4daa4539b6c00ab0819216ba77
edsagit <57862867+edsagit@users.noreply.github.com>
2021-12-22 01:59:28 +0000

    Add files via upload

--- js/lines.js
+++ js/lines.js
@@ -3,13 +3,13 @@ AFRAME.registerComponent('run', {
 
         // MEMBRANE
         markerMembranePosition = new THREE.Vector3();
-        markerMembraneBpmPosition = new THREE.Vector3();
+        markerMembraneOctavesPosition = new THREE.Vector3();
         markerMembraneFreqPosition = new THREE.Vector3();
 
         // line bpm geometry
-        this.lineBpmGeometry = new THREE.Geometry();
-        this.lineBpmGeometry.vertices.push(new THREE.Vector3(-1, -1, -1));
-        this.lineBpmGeometry.vertices.push(new THREE.Vector3(1, 1, 1));
+        this.lineOctavesGeometry = new THREE.Geometry();
+        this.lineOctavesGeometry.vertices.push(new THREE.Vector3(-1, -1, -1));
+        this.lineOctavesGeometry.vertices.push(new THREE.Vector3(1, 1, 1));
 
         // line freq geometry
         this.lineFrequencyGeometry = new THREE.Geometry();
@@ -35,19 +35,19 @@ AFRAME.registerComponent('run', {
         this.material = new THREE.LineDashedMaterial({ color: "white", dashSize: 0.3, gapSize: 0.2 });
 
         // assign lines
-        this.lineBpm = new THREE.Line(this.lineBpmGeometry, this.material);
+        this.lineOctaves = new THREE.Line(this.lineOctavesGeometry, this.material);
         this.lineFreq = new THREE.Line(this.lineFrequencyGeometry, this.material);
         this.lineCount = new THREE.Line(this.lineCountGeometry, this.material);
         this.lineDecay = new THREE.Line(this.lineDecayGeometry, this.material);
 
         // compute lines for bpm line to show dashed material
-        this.lineBpm.computeLineDistances();
+        // this.lineOctaves.computeLineDistances();
 
         // instantiate scene 
         let scene = document.querySelector('a-scene').object3D;
 
         // add previously created lines to the scene
-        scene.add(this.lineBpm);
+        scene.add(this.lineOctaves);
         scene.add(this.lineFreq);
         scene.add(this.lineCount);
         scene.add(this.lineDecay);
@@ -56,15 +56,15 @@ AFRAME.registerComponent('run', {
     tick: function (time, deltaTime) {
         // get points positions
         markerMembrane.object3D.getWorldPosition(markerMembranePosition);
-        markerMembraneBpm.object3D.getWorldPosition(markerMembraneBpmPosition);
+        markerMembraneOctaves.object3D.getWorldPosition(markerMembraneOctavesPosition);
         markerMembraneFreq.object3D.getWorldPosition(markerMembraneFreqPosition);
         markerMetal.object3D.getWorldPosition(markerMetalPosition);
         markerMetalCount.object3D.getWorldPosition(markerMetalCountPosition);
         markerMetalDecay.object3D.getWorldPosition(markerMetalDecayPosition);
 
         // line from membrane to bpm
-        this.lineBpmGeometry.vertices[0] = markerMembranePosition;
-        this.lineBpmGeometry.vertices[1] = markerMembraneBpmPosition;
+        this.lineOctavesGeometry.vertices[0] = markerMembranePosition;
+        this.lineOctavesGeometry.vertices[1] = markerMembraneOctavesPosition;
 
         // line from membrane to freq
         this.lineFrequencyGeometry.vertices[0] = markerMembranePosition;
@@ -79,17 +79,17 @@ AFRAME.registerComponent('run', {
         this.lineDecayGeometry.vertices[1] = markerMetalDecayPosition;
 
         // update vertices
-        this.lineBpmGeometry.verticesNeedUpdate = true;
+        this.lineOctavesGeometry.verticesNeedUpdate = true;
         this.lineFrequencyGeometry.verticesNeedUpdate = true;
         this.lineCountGeometry.verticesNeedUpdate = true;
         this.lineDecayGeometry.verticesNeedUpdate = true;
 
         // check if markers are visible in order to only draw the lines when their visibility is true
         // line membrane bpm visibility
-        if (markerVisible["membrane"] && markerVisible["membraneBPM"]) {
-            this.lineBpm.visible = true;
+        if (markerVisible["membrane"] && markerVisible["membraneOctaves"]) {
+            this.lineOctaves.visible = true;
             // console.log(this.p0.distanceTo(this.p1));    // PRINT LINE DISTANCE between vector this.p0 and this.p1
-        } else { this.lineBpm.visible = false; }
+        } else { this.lineOctaves.visible = false; }
         // line membrane freq visibility
         if (markerVisible["membrane"] && markerVisible["membraneFREQ"]) {
             this.lineFreq.visible = true;
@@ -102,8 +102,8 @@ AFRAME.registerComponent('run', {
         } else { this.lineCount.visible = false; }
         // line metal decay visibility
         if (markerVisible["metal"] && markerVisible["metalDecay"]) {
-            this.lineCount.visible = true;
+            this.lineDecay.visible = true;
             // console.log(this.p0.distanceTo(this.p2));    // PRINT LINE DISTANCE between vector this.p0 and this.p2
-        } else { this.lineCount.visible = false; }
+        } else { this.lineDecay.visible = false; }
     }
 });
